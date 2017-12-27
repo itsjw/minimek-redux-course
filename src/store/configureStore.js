50b99d8aab002c50294f65088de94108d4173897
Afshin Mokhtari <afshinator@hotmail.com>
2017-12-26 17:34:20 -0800

    Add reducer hot module reloading

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -16,6 +16,15 @@ export default function configureStore(preloadedState) {
   const composedEnhancer = composeWithDevTools(...storeEnhancers);
 
   const store = createStore(rootReducer, preloadedState, composedEnhancer);
+  
+  if (process.env.NODE_ENV !== "production") {
+    if (module.hot) {
+      module.hot.accept("../reducers/rootReducer", () => {
+        const newRootReducer = require("../reducers/rootReducer").default;
+        store.replaceReducer(newRootReducer);
+      });
+    }
+  }
 
   return store;
 }
