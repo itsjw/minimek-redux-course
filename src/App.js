bc56e4b4ca369e113013d9402d1a2ef7e7cf1db7
Afshin Mokhtari <afshinator@hotmail.com>
2017-12-27 17:20:17 -0800

    absolute imports

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import { Header, Container } from "semantic-ui-react";
-import TabBarContainer from "./features/tabs/TabBarContainer";
+
 import "./App.css";
 
-import SampleComponent from "./SampleComponent";
+import TabBarContainer from "features/tabs/TabBarContainer";
+import UnitInfo from "features/unitInfo/UnitInfo";
+import Pilots from "features/pilots/Pilots";
+import Mechs from "features/mechs/Mechs";
+import UnitOrganization from "features/unitOrganization/UnitOrganization";
 
 class App extends Component {
-    render() {
-        const tabs = [
-            {name : "unitInfo", label : "Unit Info"},
-            {name : "pilots", label : "Pilots"},
-            {name : "mechs", label : "Mechs"},
-            {name : "unitOrganization", label : "Unit Organization"}
-        ];
+  render() {
+    const tabs = [
+      { name: "unitInfo", label: "Unit Info", component: UnitInfo },
+      { name: "pilots", label: "Pilots", component: Pilots },
+      { name: "mechs", label: "Mechs", component: Mechs },
+      { name: "unitOrganization", label: "Unit Organization", component: UnitOrganization }
+    ];
 
-        return (
-            <div className="App">
-                <div className="App-header">
-                    <Header inverted as="h1">Project Mini-Mek</Header>
-                </div>
-                <Container>
-                    <TabBarContainer tabs={tabs} size="massive" />
-                </Container>
-            </div>
-        );
-    }
+    return (
+      <div className="App">
+        <div className="App-header">
+          <Header inverted as="h1">
+            Project Mini-Mek
+          </Header>
+        </div>
+        <Container>
+          <TabBarContainer tabs={tabs} size="massive" />
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/features/tabs/TabBar.jsx
+++ src/features/tabs/TabBar.jsx
@@ -1,24 +1,32 @@
 import React from "react";
-import { Menu } from "semantic-ui-react";
+import {Menu} from "semantic-ui-react";
 
 import Tab from "./Tab";
 
-const TabBar = props => {
-  const { tabs, currentTab, onTabClick, ...otherProps } = props;
+const TabBar = (props) => {
+    const {tabs, currentTab, onTabClick, ...otherProps} = props;
 
-  const tabItems = tabs.map(tabInfo => {
-    const { name, label } = tabInfo;
+    const tabItems = tabs.map(tabInfo => {
+        const {name, label} = tabInfo;
 
-    return <Tab key={name} name={name} label={label} active={currentTab === name} onClick={onTabClick} />;
-  });
+        return (
+            <Tab
+                key={name}
+                name={name}
+                label={label}
+                active={currentTab === name}
+                onClick={onTabClick}
+            />
+        );
+    });
 
-  return (
-    <div>
-      <Menu tabular attached="top" {...otherProps}>
-        {tabItems}
-      </Menu>
-    </div>
-  );
-};
+    return (
+        <div>
+            <Menu tabular attached="top" {...otherProps}>
+                {tabItems}
+            </Menu>
+        </div>
+    )
+}
 
 export default TabBar;
